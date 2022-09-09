59ecb2d8603263964a8cb49548d2ba8650180af2
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2022-09-09 08:02:15 -0300

    Project finalizado

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log
+let sliderElement = document.querySelector("#slider")
+let buttonElement = document.querySelector("#button")
+
+let sizePassword = document.querySelector("#valor")
+let password = document.querySelector("#password")
+
+let containerPassword = document.querySelector("#container-password")
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%¨&*"
+let novaSenha = ""
+
+sizePassword.innerHTML = sliderElement.ariaValue
+
+sliderElement.oninput =function(){
+    sizePassword.innerHTML = this.value
+}
+
+function generatePassword(){
+    alert("VAMOS GERAR A SENHA")
+}
+function generatePassword(){
+  let pass = ""
+
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+   pass += charset.charAt(Math.floor(Math.random() * n))
+   
+  }
+  containerPassword.classList.remove("hide")
+  password.innerHTML = pass
+  novaSenha = pass
+}
+
+function copyPassword(){
+    alert("Senha copiada com Sucesso!")
+    navigator.clipboard.writeText(novaSenha)
+}
